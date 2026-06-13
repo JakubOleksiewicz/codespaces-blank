97e2732531710060d8a38bbdb09ccbc8faa4b237
JakubOleksiewicz <jakub.oleksiewicz@student.wsb.edu.pl>
2026-06-13 12:27:14 +0000

    Complete program and add test

--- main.cs
+++ main.cs
@@ -58,29 +58,57 @@ class Program
 
     class Skeleton : NPC, IAttack
     {
-        public Skeleton(int id, string name) : base(id, name, 0, 0, "Neutral")
-        {}
-        public override void ShowInfo()
+        public Skeleton(int id, string name) : base(id, name, 40, 8, "Agresywny")
         {}
         public void Attack(Character target)
-        {}
+        {
+            Console.WriteLine("- " + Name + " atakuje koscia.");
+            target.TakeDamage(Damage);
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Szkielet | ID: " + Id + " | Nazwa: " + Name + " | HP: " + Hp);
+        }
     }
 
     class Ork : NPC, IAttack
     {
-        public Ork(int id, string name) : base(id, name, 0, 0, "Neutral")
-        {}
-        public override void ShowInfo()
+        public Ork(int id, string name) : base(id, name, 70, 12, "Agresywny")
         {}
         public void Attack(Character target)
-        {}
+        {
+            Console.WriteLine("- " + Name + " atakuje toporem.");
+            target.TakeDamage(Damage);
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine("Ork | ID: " + Id + " | Nazwa: " + Name + " | HP: " + Hp);
+        }
     }
 
     static void Main()
     {
-        Skeleton skeleton = new Skeleton(1, "Kosciotrup");
-        Ork ork = new Ork(2, "Grum");
-        
-        Console.WriteLine("Hej");
+        Skeleton skeleton1 = new Skeleton(1, "Dr. Bones");
+        Ork ork1 = new Ork(2, "Azog");
+
+        Character[] characters = { skeleton1, ork1 };
+
+        foreach (Character character in characters)
+        {
+            character.ShowInfo();
+        }
+
+        Console.WriteLine("--------------------------------------");
+
+        IAttack attacker1 = skeleton1;
+        IAttack attacker2 = ork1;
+
+        Console.WriteLine("-=-=( Bitwa: " + skeleton1.Name + " vs " + ork1.Name + " )=-=-");
+        while (ork1.Hp > 0 && skeleton1.Hp > 0)
+        {
+            attacker1.Attack(ork1);
+            attacker2.Attack(skeleton1);
+        }
+        Console.WriteLine("-=-=( Wygrywa: " + (ork1.Hp == 0 ? skeleton1.Name : ork1.Name) + " )=-=-");
     }
 }
