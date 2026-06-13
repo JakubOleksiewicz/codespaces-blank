5e6cfcea8dd278d4b5a8d34f9ed06fb349225e09
JakubOleksiewicz <jakub.oleksiewicz@student.wsb.edu.pl>
2026-06-13 11:43:02 +0000

    Skeleton

--- main.cs
+++ main.cs
@@ -1,1 +1,25 @@
-Console.WriteLine("Hej");
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    abstract class Character
+    {}
+
+    interface IAttack
+    {}
+
+    abstract class NPC : Character
+    {}
+
+    class Skeleton : NPC, IAttack
+    {}
+
+    class Ork : NPC, IAttack
+    {}
+
+    static void Main()
+    {
+        Console.WriteLine("Hej");
+    }
+}
